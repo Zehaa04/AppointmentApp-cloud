87475fa1401ed416f874fad7e068647f0371e722
Zehaa <emrahzehic@gmail.com>
2025-07-10 23:21:07 +0200

    changed start.sh so it can be used for automating the setup

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-kubectl delete namespace appointment --ignore-not-found #delete if found, ignore if not
+minikube start --addons=storage-provisioner,default-storageclass,ingress,registry
+
+kubectl delete namespace appointment --ignore-not-found
+
 ./build.sh
 ./rollout.sh
-minikube tunnel
+
+echo
+echo "Now run 'minikube tunnel' in a separate terminal."
+echo "If needed, add this to your hosts file:"
+echo "127.0.0.1 appointment.sample"
