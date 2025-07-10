6ddc2fc708cf89d3e16bdb8d233f6a3c4bb7e7b8
Zehaa <emrahzehic@gmail.com>
2025-07-10 23:52:25 +0200

    modified start.sh so it work correctly

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+minikube delete
 minikube start --addons=storage-provisioner,default-storageclass,ingress,registry
 
-kubectl delete namespace appointment --ignore-not-found
+kubectl delete namespace appointment --ignore-not-found #delete if found, ignore if not
 
 ./build.sh
 ./rollout.sh
@@ -11,3 +12,4 @@ echo
 echo "Now run 'minikube tunnel' in a separate terminal."
 echo "If needed, add this to your hosts file:"
 echo "127.0.0.1 appointment.sample"
+sleep 2
